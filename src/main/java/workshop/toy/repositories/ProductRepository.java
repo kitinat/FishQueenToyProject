5d72543bf16f50a83575f22950c83dae88841139
tanetg <tanetg@gmail.com>
2018-04-24 17:18:06 +0700

    Fixed conflict of pom file

--- src/main/java/workshop/toy/repositories/ProductRepository.java
+++ src/main/java/workshop/toy/repositories/ProductRepository.java
@@ -2,7 +2,8 @@ package workshop.toy.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import workshop.toy.models.Age;
+import workshop.toy.models.Product;
 
-public class ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository {
 
 }
