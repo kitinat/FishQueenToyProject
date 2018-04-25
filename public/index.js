8f9131e74aecc4dbb764a048601c18ff04da0501
spiderman <spiderman@spidermans-Mac-mini.local>
2018-04-25 15:25:14 +0700

    add function show product card

--- public/index.js
+++ public/index.js
@@ -20,11 +20,15 @@
                 });
     }
     function getProductInit(){
+                renderProduct(10,1,1,1,1);
                 var age = "all";
                 var gender = "all";
                 $.getJSON("rest/product/"+age+"/"+gender, function (data) {
                 $( "#searchResult" ).text(data.length+" Toy Found");
+                //renderProduct(data.length,data.id,data.product_name,data.price,data.availability);
+
                 });
+
 
     }
     function getProduct(age,gender){
@@ -45,4 +49,23 @@
     getAge();
     getGender();
     getProductInit();
+
+
+    function renderProduct(searchResult,id,product_name,price,availability) {
+    for(var i=1 ; i <= searchResult ; i++){
+        var card  = "<div class='card float-left' style='width: 18rem;'>";
+            card += "<img class='card-img-top' src='"+id[i]+".jpg' alt='Card image cap'>";
+            card += "<div class='card-body'>";
+            card += "<h5 class='card-title' id='productName'>"+product_name[i]+"</h5>";
+            card += "<p class='card-text' id='productDetail'>"+price[i]+"</p>";
+            card += "<a href='product_detail/"+price[i]+"' class='btn btn-chelsea float-right'>Product Detail</a>";
+            card += "</div>";
+            card += "</div>";
+        $("#searchResultArea").append(card);     // Append new elements
+    }
+
+    }
+
+
+
 
