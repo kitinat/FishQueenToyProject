7c968b17f7622b0a604c33c6e6b2b7e576e1edb9
gunner1886 <johndoe@example.com>
2018-04-25 08:59:49 +0700

    replace file

--- src/main/java/workshop/toy/repositories/ProductRepository.java
+++ src/main/java/workshop/toy/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package workshop.toy.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import workshop.toy.models.Age;
+import workshop.toy.models.Product;
 
 public class ProductRepository extends CrudRepository<Product, Integer> {
 
