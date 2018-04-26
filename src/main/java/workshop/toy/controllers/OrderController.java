22137905c9abe3e78e1f49d4ac07efc27e8e9c13
gunner1886 <johndoe@example.com>
2018-04-26 13:30:53 +0700

    fix code

--- src/main/java/workshop/toy/controllers/OrderController.java
+++ src/main/java/workshop/toy/controllers/OrderController.java
@@ -2,17 +2,17 @@ package workshop.toy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import workshop.toy.models.Cart;
-import workshop.toy.models.CartItem;
-import workshop.toy.models.ManageCart;
-import workshop.toy.models.ShippingAddress;
+import workshop.toy.models.*;
+import workshop.toy.repositories.OrderDRepository;
 import workshop.toy.repositories.OrderHRepository;
 import workshop.toy.repositories.ProductRepository;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/rest/order")
+@RequestMapping("/rest")
 public class OrderController {
 
     @Autowired
@@ -22,6 +22,8 @@ public class OrderController {
     ProductRepository productRepository;
     @Autowired
     OrderHRepository orderHRepository;
+    @Autowired
+    OrderDRepository orderDRepository;
 
 
     @GetMapping("/stock/{cartId}")
@@ -37,6 +39,46 @@ public class OrderController {
             }
         }
         return isAvailable;
+    }
+
+    @PostMapping("/stock/cart/{cartId}")
+    public Cart updateCartWithCurrentStock(@PathVariable String cartId) {
+        Cart cart = manageCart.getCart(cartId);
+        String isAvailable = "Y";
+        for (Map.Entry<String, CartItem> entry: cart.getItems().entrySet()){
+            CartItem cartItem = entry.getValue();
+            int stockQty = productRepository.getStockQtyById(Integer.parseInt(cartItem.getProduct_id()));
+            if(cartItem.getQty() > stockQty){
+                cartItem.setQty(stockQty);
+                if(stockQty == 0){
+                    cartItem.setAvailability("Out of Stock");
+                }
+            }
+        }
+        return cart;
+    }
+
+    @PostMapping("/stock/product/{cartId}")
+    public void updateStock(@PathVariable String cartId) {
+        Cart cart = manageCart.getCart(cartId);
+        for (Map.Entry<String, CartItem> entry: cart.getItems().entrySet()){
+            CartItem cartItem = entry.getValue();
+            int stockQty = productRepository.getStockQtyById(Integer.parseInt(cartItem.getProduct_id()));
+            if(cartItem.getQty() <= stockQty){
+               productRepository.updateStockQty(Integer.parseInt(cartItem.getProduct_id()), (stockQty-cartItem.getQty()));
+            }
+        }
+    }
+
+    @PostMapping("/orderH")
+    public OrderH createOrderH(@RequestBody OrderH orderH) {
+        orderH.setOrderDate(new Timestamp(System.currentTimeMillis()));
+        return orderHRepository.save(orderH);
+    }
+
+    @PostMapping("/orderD")
+    public OrderD createOrderH(@PathVariable OrderD orderD) {
+        return orderDRepository.save(orderD);
     }
 
 
