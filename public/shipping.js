d734bf14c1d21f032b369987fedab20aa3775d29
blackwidow <blackwidow@blackwidows-Mac-mini.local>
2018-04-27 15:24:53 +0700

    FIXED

--- public/shipping.js
+++ public/shipping.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
 });
 
 $("#confirm").click(function(){
-    alert('dddddd');
+    //alert('dddddd');
     var fullname = $("#fullname").val();
     var address1 = $("#address1").val();
     var address2 = $("#address2").val();
     var city = $("#city").val();
     var province = $("#province").val();
     var postcode = $("#post_code").val();
-    var email = $("#post_code").val();
+    var email = $("#email").val();
     var cartId = sessionStorage.getItem("cart_id");
 
     var data = {
@@ -52,7 +52,7 @@ function createOrderD(cart_id,orderH){
             "data" : JSON.stringify(orderH),
             "success" : function() {
                     //alert('send mail');
-                    sendEmail(cart_id,orderH)
+                    sendEmail(cart_id,orderH);
             },
             "error" : function(response) {
                     //alert ('Fail to create order');
@@ -69,11 +69,11 @@ function sendEmail(cart_id,orderH){
             "contentType" : "application/json; charset=utf-8",
             "data" : JSON.stringify(cart_id,orderH),
             "success" : function() {
-                    alert('sent ja');
+                    //alert('sent ja');
                     window.open("thankyou.html","_self");
             },
             "error" : function(response) {
-                    alert ('Fail to send e-mail');
+                    //alert ('Fail to send e-mail');
                     window.open("thankyou.html","_self");
             }
         });
