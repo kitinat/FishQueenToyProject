cfd1a2386beb12b9ed6d0a3b10f2611d9b1769a3
gunner1886 <johndoe@example.com>
2018-04-27 15:50:45 +0700

    remove import class

--- src/main/java/workshop/toy/repositories/OrderHRepository.java
+++ src/main/java/workshop/toy/repositories/OrderHRepository.java
@@ -1,13 +1,8 @@
 package workshop.toy.repositories;
 
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import workshop.toy.models.OrderH;
 
-import java.sql.Timestamp;
-
-public interface OrderHRepository  extends CrudRepository<OrderH, Integer> {
+public interface OrderHRepository extends CrudRepository<OrderH, Integer> {
 
 }
